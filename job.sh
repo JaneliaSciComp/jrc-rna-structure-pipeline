ac744683654f7ad9042a20ffda4f38276e8d846a
porebskip <4824691+hokinus@users.noreply.github.com>
2025-09-29 17:20:14 -0400

    Updated job.sh

--- job.sh
+++ job.sh
@@ -8,7 +8,7 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 python ${SCRIPT_DIR}/get_pdb_data.py --start_date now-1w --stop_date now --search_type polymer_type --search_term RNA --output_dir rna_structures_all &&
 python ${SCRIPT_DIR}/filter_by_composition.py rna_structures_all rna_structures &&
 python ${SCRIPT_DIR}/split_chains.py rna_structures split_chains &&
-python ${SCRIPT_DIR}/get_xyz_data.py &&
+python ${SCRIPT_DIR}/get_xyz_data.py split_chains raw_pdb_xyz_data.pkl --id-source auth &&
 python ${SCRIPT_DIR}/filter_by_interC1.py &&
 python ${SCRIPT_DIR}/get_pub_dates_and_dedupe.py &&
 python ${SCRIPT_DIR}/align_sequence.py &&
