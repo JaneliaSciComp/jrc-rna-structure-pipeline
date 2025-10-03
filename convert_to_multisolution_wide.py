898b41615ea976f9549dae48a617cd7991662726
porebskip <4824691+hokinus@users.noreply.github.com>
2025-10-03 17:06:42 -0400

    Tiding up output

--- convert_to_multisolution_wide.py
+++ convert_to_multisolution_wide.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def to_wide(result_solution, select=None):
+def to_wide(result_solution, fill_coordinates=None, select=None):
     """
     Convert long format solution dataframe to wide format and save to CSV, keeping only specified atoms.
 
@@ -30,16 +30,29 @@ def to_wide(result_solution, select=None):
     # result_solution_selected = result_solution[other_columns + selected_columns]
 
     # Use atom coordinates and solution_id as multiindex for columns
-    result_solution_selected = (
-        result_solution[keep + list(selected_columns)]
-        .set_index(keep)
-        .unstack("solution_id")
+    result_solution_selected = result_solution[keep + list(selected_columns)]
+
+    result_solution_selected = result_solution_selected.set_index(keep).unstack(
+        "solution_id", sort=False
     )
+    result_solution_selected = result_solution_selected.sort_index(level=[1, 0], axis=1)
+    # Fill missing coordinates if requested
+    if fill_coordinates is not None:
+        # We need to convert to string first to apply float formatting, doing it here
+        # so the columns are already aligned and filled with nans
+        coordinate_mask = result_solution_selected.columns.get_level_values(
+            0
+        ).str.endswith(("_x", "_y", "_z"))
+        coordinate_columns = result_solution_selected.columns[coordinate_mask]
+        result_solution_selected[coordinate_columns] = result_solution_selected[
+            coordinate_columns
+        ].map(lambda x: f"{x:.3f}" if pd.notnull(x) else fill_coordinates)
 
     # Map multilevel column names to {x,y,z}_index
     result_solution_selected.columns = [
         f"{i}_{j}" if j != "" else f"{i}" for i, j in result_solution_selected.columns
     ]
+
 
     # Sort by resid within each group
     result_solution_selected = result_solution_selected.sort_values(
@@ -84,13 +97,39 @@ if __name__ == "__main__":
         default=None,
         help="Select atoms starting with this prefix",
     )
+    parser.add_argument(
+        "--fill-coordinates",
+        type=str,
+        default=None,
+        help="Fill missing values with this string",
+    )
+
+    parser.add_argument(
+        "--add-column",
+        nargs=2,
+        action="append",
+        metavar=("column_name", "value"),
+        help="Add a new column with the given name and value",
+    )
+    parser.add_argument(
+        "--drop-column", nargs="+", type=str, help="Drop these columns before saving"
+    )
 
     args = parser.parse_args()
     input_file = Path(args.input_file)
     output_file = Path(args.output_file)
     result_solution = pd.read_csv(input_file)
-    result_solution_selected = to_wide(result_solution, select=args.select)
+    result_solution_selected = to_wide(
+        result_solution, fill_coordinates=args.fill_coordinates, select=args.select
+    )
 
+    if args.add_column:
+        for col_name, value in args.add_column:
+            result_solution_selected[col_name] = value
+    if args.drop_column:
+        result_solution_selected = result_solution_selected.drop(
+            columns=args.drop_column, errors="ignore"
+        )
     result_solution_selected.to_csv(
         output_file,
         float_format="%.3f",
